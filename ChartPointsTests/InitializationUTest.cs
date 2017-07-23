9f9841d3236b4c52167b3deafdbf52edc4dc4580
aleksey_m_t <aleksey_m_t@WE>
2017-07-23 16:01:01 +0300

    Added UTest project
    Supports attaching to devenv experimental instance (NEEDED: add timed out choice)
    Supports acquisition of ChartPointsPackage instance

---
+++ ChartPointsTests/InitializationUTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EnvDTE;
+using Microsoft.VSSDK.Tools.VsIdeTesting;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using ChartPoints;
+
+namespace ChartPointsTests
+{
+  [TestClass]
+  public class InitializationUTest
+  {
+    public static ChartPointsPackage chartPntPackage;
+    [ClassInitialize]
+    [HostType("VS IDE")]
+    public static void TestClassInitialize(TestContext testContext)
+    {
+      var dte = VsIdeTestHostContext.Dte;
+      if (MessageBox.Show("Attach VS Experimental instance to debugger?", "Attach to debugger", MessageBoxButtons.OKCancel) == DialogResult.OK)
+      {
+        var thisDte = (DTE) Marshal.GetActiveObject("VisualStudio.DTE.14.0");
+        EnvDTE.Processes processes = thisDte.Debugger.LocalProcesses;
+        foreach (EnvDTE.Process proc in processes)
+        {
+          if (proc.Name.IndexOf("devenv.exe") != -1)
+            proc.Attach();
+        }
+      }
+      var shellService = VsIdeTestHostContext.ServiceProvider.GetService(typeof(SVsShell)) as IVsShell;
+      Guid packageGuid = new Guid("a6b9b46f-0163-4255-807e-b3e04aa6ca70");
+      Microsoft.VisualStudio.Shell.Interop.IVsPackage package;
+      int res = shellService.LoadPackage(ref packageGuid, out package);
+      InitializationUTest.chartPntPackage = (ChartPointsPackage)package;
+    }
+
+    [TestMethod]
+    [HostType("VS IDE")]
+    [TestProperty("VsHiveName", "14.0Exp")]
+    public void TestThePackageAcquisition()
+    {
+      Assert.AreNotEqual(InitializationUTest.chartPntPackage, null);
+    }
+  }
+}
